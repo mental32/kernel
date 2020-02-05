3561e4024fb4ca5c147f387a1ec8b6454d87c6da
mental <m3nta1@yahoo.com>
2020-02-05 15:33:40 +0000

    refactor and document serial driver

--- drivers/serial/src/lib.rs
+++ drivers/serial/src/lib.rs
@@ -1,28 +1,45 @@
 #![no_std]
-// #![forbid(missing_docs)]
+#![forbid(missing_docs)]
+
+//! Primitive serial IO driver.
+//!
+//! # Examples
+//!
+//! ```
+//! use serial::sprintln;
+//! sprintln!("{:?}", "Hello, World!");
+//! ```
 
 use {core::fmt::Write, lazy_static::lazy_static, spin::Mutex, uart_16550::SerialPort};
 
-pub const SERIAL_IO_PORT: u16 = 0x3F8;
+/// Default serial io port for x86 systems.
+pub const DEAFULT_SERIAL_IO_PORT: u16 = 0x3F8;
 
 lazy_static! {
-    pub static ref DEFAULT_SERIAL: Mutex<SerialPort> = {
-        let mut port = unsafe { SerialPort::new(SERIAL_IO_PORT) };
+    /// Global static refrence to a ``Mutex<SerialPort>`` using the ``DEAFULT_SERIAL_IO_PORT`` port.
+    pub static ref GLOBAL_DEFAULT_SERIAL: Mutex<SerialPort> = {
+        let mut port = unsafe { SerialPort::new(DEAFULT_SERIAL_IO_PORT) };
         port.init();
         Mutex::new(port)
     };
 }
 
+/// Write formatted arguments into a serial port and panic on errors.
 #[inline]
 pub fn serial_println(port: &mut SerialPort, args: core::fmt::Arguments) {
     port.write_fmt(args).unwrap();
 }
 
+/// Helper macros that deal with serial IO.
 #[macro_export]
 macro_rules! sprintln {
     ($($arg:tt)*) => {
-        let mut handle = (*$crate::DEFAULT_SERIAL).lock();
+        let mut handle = (*$crate::GLOBAL_DEFAULT_SERIAL).lock();
         $crate::serial_println(&mut handle, format_args!($($arg)*));
         core::mem::drop(handle);
+    };
+
+    ($handle:ident, $($arg:tt)*) => {
+        $crate::serial_println(&mut $handle, format_args!($($arg)*));
     }
 }
