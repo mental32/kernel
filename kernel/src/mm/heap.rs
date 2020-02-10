f4ffa74eb3f2e0888d85dc180a8f155731254eec
mental <m3nta1@yahoo.com>
2020-02-10 07:44:01 +0000

    Restructure mm

---
+++ kernel/src/mm/heap.rs
@@ -0,0 +1,32 @@
+use {
+    alloc::alloc::{GlobalAlloc, Layout},
+    core::ptr::null_mut,
+};
+
+use linked_list_allocator::LockedHeap;
+
+/// The virtual address of where the heap will be mapped.
+pub const HEAP_START: usize = 0x_4444_4444_0000;
+
+/// The amount of memory the heap will be grown by in typical cases.
+pub const HEAP_STEP: usize = 4 * 1024; // 4 KiB
+
+#[global_allocator]
+pub static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+pub struct Dummy;
+
+unsafe impl GlobalAlloc for Dummy {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        null_mut()
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        panic!("dealloc should be never called")
+    }
+}
+
+#[alloc_error_handler]
+pub fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
+}
--- kernel/src/mm/mod.rs
+++ kernel/src/mm/mod.rs
@@ -1,29 +1,6 @@
-use {
-    alloc::alloc::{GlobalAlloc, Layout},
-    core::ptr::null_mut,
-};
+use {spin::RwLock, x86_64::structures::paging::PageTable};
 
-use linked_list_allocator::LockedHeap;
+mod heap;
 
-pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
-
-#[global_allocator]
-pub static ALLOCATOR: LockedHeap = LockedHeap::empty();
-
-pub struct Dummy;
-
-unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        null_mut()
-    }
-
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        panic!("dealloc should be never called")
-    }
-}
-
-#[alloc_error_handler]
-pub fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
-    panic!("allocation error: {:?}", layout)
-}
+#[no_mangle]
+pub static PAGE_MAP_LEVEL_4: RwLock<PageTable> = RwLock::new(PageTable::new());
