9135b453c2c44ddce7481a9de690186924875d09
mental <m3nta1@yahoo.com>
2020-02-11 10:25:16 +0000

    refactor threading

--- kernel/src/sched/thread.rs
+++ kernel/src/sched/thread.rs
@@ -5,15 +5,12 @@ pub(crate) type ThreadIdent = u64;
 #[derive(Debug, PartialEq)]
 #[repr(C)]
 pub(crate) struct ThreadControlBlock {
-    stack_addr: Option<VirtAddr>,
+    stack: Option<VirtAddr>,
     ident: ThreadIdent,
 }
 
 impl ThreadControlBlock {
     pub fn new(ident: ThreadIdent) -> Self {
-        Self {
-            ident,
-            stack_addr: None,
-        }
+        Self { ident, stack: None }
     }
 }
