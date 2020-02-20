7c22d0210113d3de1e289334cdafc2b5571bd41f
mental <m3nta1@yahoo.com>
2020-02-20 04:27:25 +0000

    rewrite boot frame

--- kernel/src/mm/boot_frame.rs
+++ kernel/src/mm/boot_frame.rs
@@ -40,49 +40,53 @@ fn page_range_inclusive(start: u64, stop: u64) -> PageRangeInclusive {
     Page::range_inclusive(start_page, end_page)
 }
 
-pub fn find_non_overlapping(boot_info: &BootInformation) -> (u64, u64) {
-    // Heap page range
-
+pub fn elf_areas(boot_info: &BootInformation) -> SmallVec<[(u64, u64); 16]> {
     let elf_tag = boot_info.elf_sections_tag().unwrap();
-    let section_page_ranges = {
-        let mut raw_section_page_ranges = elf_tag
-            .sections()
-            .map(|section| {
-                Some(page_range_inclusive(
-                    section.start_address(),
-                    section.end_address() + section.size(),
-                ))
-            })
-            .collect::<SmallVec<[Option<PageRangeInclusive>; 16]>>();
 
-        // Fuse contiguous ranges.
-        for index in 0..(raw_section_page_ranges.len() - 1) {
-            if raw_section_page_ranges[index].is_none() {
-                continue;
-            }
+    let mut raw_section_page_ranges = elf_tag
+        .sections()
+        .map(|section| {
+            Some(page_range_inclusive(
+                section.start_address(),
+                section.end_address() + section.size(),
+            ))
+        })
+        .collect::<SmallVec<[Option<PageRangeInclusive>; 16]>>();
 
-            let left = raw_section_page_ranges[index].unwrap();
-            let right = raw_section_page_ranges[index + 1].unwrap();
-
-            if left.end == right.start {
-                raw_section_page_ranges[index + 1] =
-                    Some(Page::range_inclusive(left.start, right.end));
-                raw_section_page_ranges[index] = None;
-            }
+    // Fuse contiguous ranges.
+    for index in 0..(raw_section_page_ranges.len() - 1) {
+        if raw_section_page_ranges[index].is_none() {
+            continue;
         }
 
-        raw_section_page_ranges
-            .iter()
-            .filter(|range| range.is_some())
-            .map(|range| range.unwrap())
-            .map(|range| {
-                (
-                    range.start.start_address().as_u64(),
-                    range.end.start_address().as_u64() + range.end.size(),
-                )
-            })
-            .collect::<SmallVec<[(u64, u64); 16]>>()
-    };
+        let left = raw_section_page_ranges[index].unwrap();
+        let right = raw_section_page_ranges[index + 1].unwrap();
+
+        if left.end == right.start {
+            raw_section_page_ranges[index + 1] = Some(Page::range_inclusive(left.start, right.end));
+            raw_section_page_ranges[index] = None;
+        }
+    }
+
+    raw_section_page_ranges
+        .iter()
+        .filter(|range| range.is_some())
+        .map(|range| range.unwrap())
+        .map(|range| {
+            (
+                range.start.start_address().as_u64(),
+                range.end.start_address().as_u64() + range.end.size(),
+            )
+        })
+        .collect::<SmallVec<[(u64, u64); 16]>>()
+}
+
+pub fn find_first_non_overlapping_free_area(
+    size: usize,
+    boot_info: &BootInformation,
+) -> (u64, u64) {
+    // Heap page range
+    let section_page_ranges = elf_areas(boot_info);
 
     let memory_map = boot_info.memory_map_tag().unwrap();
     let heap_range = {
@@ -93,15 +97,23 @@ pub fn find_non_overlapping(boot_info: &BootInformation) -> (u64, u64) {
         // It'll only yield areas that are listed as available
         // By the multiboot tag, not all areas that get listed.
         for area in memory_map.memory_areas() {
-            let area_start = area.start_address();
-            let area_end = area.end_address();
-            let area = page_range_exclusive(area_start, area_end);
+            let area_start: usize = area.start_address() as usize;
+            let area_end: usize = area.end_address() as usize;
 
-            if section_page_ranges
-                .iter()
-                .all(|(start, stop)| (area_start <= *stop) && (*start >= area_end))
-            {
-                heap_range = Some(area);
+            for section_start in (area_start..area_end).step_by(size) {
+                let section_end = section_start + size;
+                let section = page_range_exclusive(section_start as u64, section_end as u64);
+
+                if section_page_ranges.iter().all(|(start, stop)| {
+                    ((section_start as u64) <= *stop) && (*start >= (section_end as u64))
+                }) {
+                    heap_range = Some(section);
+                    break;
+                }
+            }
+
+            if heap_range.is_some() {
+                break;
             }
         }
 
@@ -115,7 +127,7 @@ pub fn find_non_overlapping(boot_info: &BootInformation) -> (u64, u64) {
 
     // When an allocation call returns 0x00 as a pointer
     // It's treated as an allocation failure, even though
-    // In some cases an area starting at phys addr 0x00 is
+    // In some cases an area starting at addr 0x00 is
     // Possible.
     if heap_start == 0 {
         heap_start += 8; // 8 byte alignment
