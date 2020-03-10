a7dd47c307c76e0046fd4798d98f3f1f9a82cd4c
mental <m3nta1@yahoo.com>
2020-03-10 20:53:03 +0000

    bugfix acpi::interrupts::Apic refers to the entire APIC not BSP Lapic

--- kernel/src/dev/apic/lapic.rs
+++ kernel/src/dev/apic/lapic.rs
@@ -12,18 +12,14 @@ use x86_64::{
     VirtAddr,
 };
 
-pub struct Lapic<'a> {
-    pub inner: &'a ApicInner,
+pub struct Lapic {
+    register_page: Page,
 }
 
-impl<'a> Lapic<'a> {
-    pub fn new(acpi: &'a Acpi) -> KernelResult<Lapic> {
-        let apic = match &acpi.interrupt_model {
-            Some(InterruptModel::Apic(apic)) => apic,
-            _ => return Err(KernelException::Acpi(AcpiError::ApicNotSupported)),
-        };
-
-        Ok(Self { inner: apic })
+impl Lapic {
+    pub fn new(local_apic_address: u64) -> Lapic {
+        let register_page = Page::containing_address(VirtAddr::new(local_apic_address));
+        Self { register_page }
     }
 
     pub unsafe fn init(&mut self) -> KernelResult<()> {
@@ -31,22 +27,19 @@ impl<'a> Lapic<'a> {
             .try_lock()
             .expect("Unable to access the memory manager during APIC initialization.");
 
-        let lapic_base = self.inner.local_apic_address;
-        let apic_register_page = Page::containing_address(VirtAddr::new(lapic_base));
-
-        if memory_manager.page_is_mapped(apic_register_page) {
+        if memory_manager.page_is_mapped(self.register_page) {
             return Err(KernelException::PageAlreadyMapped);
         }
 
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
         memory_manager
-            .map_to(apic_register_page, flags)
+            .map_to(self.register_page, flags)
             .expect("Unable to map APIC register page!")
             .flush();
 
         SPURRIOUS_INTERRUPT_VECTOR.write(
-            apic_register_page,
-            SPURRIOUS_INTERRUPT_VECTOR.read(apic_register_page)? | 0x1ff,
+            self.register_page,
+            SPURRIOUS_INTERRUPT_VECTOR.read(self.register_page)? | 0x1ff,
         )?;
 
         Ok(())
